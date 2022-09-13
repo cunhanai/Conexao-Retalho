74f7ad780ae317107c32964b28520fee103ccd6c
cunhanai <dacunhanajulia@gmail.com>
2022-09-13 11:41:16 -0300

    iniciando aplicacao

---
+++ src/main/java/br/com/entra21/retalhando/RetalhandoApplication.java
@@ -0,0 +1,13 @@
+package br.com.entra21.retalhando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetalhandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetalhandoApplication.class, args);
+	}
+
+}
