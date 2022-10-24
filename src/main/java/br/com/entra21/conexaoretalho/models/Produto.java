b583c53d856dd8ba49079ce5bb309555ffecbedf
cunhanai <dacunhanajulia@gmail.com>
2022-10-24 11:03:20 -0300

    redirecionamentos alterados

--- src/main/java/br/com/entra21/conexaoretalho/models/Produto.java
+++ src/main/java/br/com/entra21/conexaoretalho/models/Produto.java
@@ -3,6 +3,7 @@ package br.com.entra21.conexaoretalho.models;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class Produto implements Serializable {
 	private String descricaoo;
 	
 	private boolean disponivel = true;
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Ong ong;
 
 	public long getId() {
